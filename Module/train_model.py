5780843db1d27980d3a1033291c5dea0c641f220
exilesaber <1124235391@qq.com>
2025-02-02 21:19:19 +0800

    增加cuda判断，gpu训练

--- Module/train_model.py
+++ Module/train_model.py
@@ -7,13 +7,19 @@ from DataLoad.image_augment import basic_augment
 
 
 def start_train(args):
-    model = SimpleDetector(num_classes=1)
+    # 设置设备，检查是否有可用的GPU，如果没有则使用CPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    # 创建模型并将模型迁移到指定的设备
+    model = SimpleDetector(num_classes=1).to(device)
 
     if args.augment == "basic":
         augment = basic_augment
     else:
         augment = None
 
+    # 获取数据加载器
     train_loader, val_loader, test_loader = get_dataloader(args, augment)
 
     # 损失函数
@@ -23,16 +29,23 @@ def start_train(args):
     # 优化器
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+    # 训练循环
     for epoch in range(args.epochs):
+        model.train()  # 训练模式
         for images, boxes, labels, masks in train_loader:
+            # 将数据移动到GPU
+            images = images.to(device)
+            boxes = boxes.to(device)
+            labels = labels.to(device)
+            masks = masks.to(device)
+
             # 前向传播
             class_logits, bbox_preds = model(images)
 
+            # 移除最后一个维度
             class_logits = class_logits.squeeze(-1)
             
             # 计算损失
-            # print(class_logits.shape)  # 应该是 [batch_size, num_boxes, num_classes]
-            # print(labels.shape)        # 应该是 [batch_size, num_boxes]
             cls_loss = classification_loss_fn(class_logits[masks], labels[masks])
             reg_loss = regression_loss_fn(bbox_preds[masks], boxes[masks])
             loss = cls_loss + reg_loss
@@ -42,4 +55,6 @@ def start_train(args):
             loss.backward()
             optimizer.step()
 
+        # 打印每个epoch的损失
         print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+
