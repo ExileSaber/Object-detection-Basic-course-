d5f3b7767ecdf23cba20fd9152ef72e793846377
exilesaber <1124235391@qq.com>
2025-02-02 12:47:53 +0800

    增加训练验证测试集划分

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 
-from DataLoad.dataloader import PedestrianDataset, collate_fn
+from DataLoad.dataloader import get_dataloader
 from utils.read_cfg import load_yaml_as_namespace
 from utils.func import get_absolute_path
 
@@ -13,14 +13,23 @@ if __name__ == "__main__":
     image_folder = args.image_folder
     xml_folder = args.xml_folder
 
-    # 创建数据集
-    dataset = PedestrianDataset(image_folder, xml_folder, transform=ToTensor())
+    train_rate = args.train_rate
+    val_rate = args.val_rate
 
-    # 创建 DataLoader
-    dataloader = DataLoader(dataset, batch_size=2, collate_fn=collate_fn, shuffle=True)
+    train_loader, val_loader, test_loader = get_dataloader(image_folder, xml_folder, train_rate, val_rate)
 
     # 使用 DataLoader 迭代数据
-    for images, boxes, labels in dataloader:
-        print(f"Images batch shape: {images.shape}")
+    for images, boxes, labels in train_loader:
+        print(f"Train batch shape: {images.shape}")
+        print(f"Boxes: {boxes}")
+        print(f"Labels: {labels}")
+
+    for images, boxes, labels in val_loader:
+        print(f"Validation batch shape: {images.shape}")
+        print(f"Boxes: {boxes}")
+        print(f"Labels: {labels}")
+
+    for images, boxes, labels in test_loader:
+        print(f"Test batch shape: {images.shape}")
         print(f"Boxes: {boxes}")
         print(f"Labels: {labels}")
